2df4309119a7f8954110f2ea75e3c43a2364ca1d
Ifthenpay <ricardocarvalho@ifthenpay.com>
2021-06-11 10:47:31 +0100

    upgrade module version

--- versao 1.7 ou superior/ifthenpay/ifthenpay.php
+++ versao 1.7 ou superior/ifthenpay/ifthenpay.php
@@ -50,7 +50,7 @@ class Ifthenpay extends PaymentModule
     {
         $this->name = 'ifthenpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.2.1';
+        $this->version = '1.2.2';
         $this->author = 'Ifthenpay';
         $this->need_instance = 0;
         $this->bootstrap = true;
