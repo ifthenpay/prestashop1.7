a4b26dbf47dff0daec6034d82e2b6b79137de983
Ifthenpay <ricardocarvalho@ifthenpay.com>
2021-12-02 10:50:59 +0000

    change paymentUrl lenght collum

--- versao 1.7 ou superior/ifthenpay/classes/Config/IfthenpaySql.php
+++ versao 1.7 ou superior/ifthenpay/classes/Config/IfthenpaySql.php
@@ -69,7 +69,7 @@ class IfthenpaySql implements InstallerInterface
           'ccard' => 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'ifthenpay_ccard` (
             `id_ifthenpay_ccard` int(10) unsigned NOT NULL auto_increment,
             `requestId` varchar(50) NOT NULL,
-            `paymentUrl` varchar(1000) NOT NULL,
+            `paymentUrl` varchar(1500) NOT NULL,
             `order_id` int(11) NOT NULL,
             `status` varchar(50) NOT NULL,
             PRIMARY KEY  (`id_ifthenpay_ccard`),
@@ -177,7 +177,7 @@ class IfthenpaySql implements InstallerInterface
         if ($sqlResult && strtolower($sqlResult[array_keys($sqlResult)[0]]) == strtolower(_DB_PREFIX_ . 'ifthenpay_ccard')) {
             $sqlExecute = \Db::getInstance()->executeS("SELECT COLUMN_NAME, DATA_TYPE, COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = " . "'" . _DB_PREFIX_ . "ifthenpay_ccard' AND COLUMN_NAME = 'paymentUrl'");
             if ($sqlExecute[0]['COLUMN_TYPE'] === 'varchar(250)') {
-                $sqlExecute = \Db::getInstance()->execute('ALTER TABLE ' . _DB_PREFIX_ . 'ifthenpay_ccard  MODIFY paymentUrl varchar(1000)');
+                $sqlExecute = \Db::getInstance()->execute('ALTER TABLE ' . _DB_PREFIX_ . 'ifthenpay_ccard  MODIFY paymentUrl varchar(1500)');
                 if (!$sqlExecute) {
                     throw new \Exception($this->ifthenpayModule->l('Error changing ccard table!', Utility::getClassName($this)));
                 }
