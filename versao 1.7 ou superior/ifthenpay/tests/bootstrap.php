260f008a46a011245ce389e0b7bab41f8b4cb1c2
josesoaresif <100134255+josesoaresif@users.noreply.github.com>
2022-10-07 14:53:24 +0100

    Delete versao 1.7 ou superior/ifthenpay/tests directory

--- versao 1.7 ou superior/ifthenpay/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require('C:\xampp7.2\htdocs\prestashopPrefixChange\config\config.inc.php');
-/*Dispatcher::getInstance()->dispatch();*/
-require('C:\xampp7.2\htdocs\prestashopPrefixChange\modules\ifthenpay\ifthenpay.php');
