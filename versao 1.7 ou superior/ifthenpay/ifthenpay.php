5e9aef3289ce961a9b5498725e3a158e4666682d
josesoaresif <josesoares@ifthenpay.com>
2024-10-11 12:17:43 +0000

    release: module update from ifthenpay/dev_prestashop_1.7_aio (https://github.com/ifthenpay/dev_prestashop_1.7_aio/commit/b8f30c3e3a13642198fd3ff6cf634f9c058197c0)

--- versao 1.7 ou superior/ifthenpay/ifthenpay.php
+++ versao 1.7 ou superior/ifthenpay/ifthenpay.php
@@ -50,7 +50,7 @@ class Ifthenpay extends PaymentModule
 	{
 		$this->name = 'ifthenpay';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.4.2';
+		$this->version = '1.5.0';
 		$this->author = 'Ifthenpay';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
